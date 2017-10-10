3ac13d3104a0fc9c74dfebeb72e0eea9e90ec050
erick <erick@hotmail.ca>
2017-10-09 22:47:11 -0400

    Invite users when PR merged

--- index.js
+++ index.js
@@ -2,12 +2,11 @@ module.exports = (robot) => {
   console.log('App started...')
 
   robot.on('pull_request.closed', inviteMember);
-
   
   async function inviteMember(context) {
       
     const isMerged = context.payload.pull_request.merged;
-    const isOrg = context.payload.pull_request.head.repo.owner.type === 'Organization';
+    const isOrg = context.payload.repository.owner.type === 'Organization';
 
     if (context.isBot || !isMerged || !isOrg) {
       robot.log('This pull request belongs to a non-organization repo, has made by a bot user, or has been closed without merging. :(')
@@ -15,16 +14,26 @@ module.exports = (robot) => {
     }
 
     const memberPayload = {
-      org: context.payload.pull_request.head.repo.owner.login,
+      org: context.payload.repository.owner.login,
       username: context.payload.pull_request.user.login,
       role: 'member'
     }
 
-    robot.log('This pull request has been merged! :)');
+    let response;
+    try {
+      response = await context.github.orgs.addOrgMembership(memberPayload);
 
-    //const data = await context.github.orgs.addOrgMembership(memberPayload).data;
+      if (response.state === 'pending') {
+        robot.log('User is already in organization! :)')
+      } else {
+        robot.log('User has been added to the organization! :)');
+      }
 
-    //robot.log(data);
+    } catch(e) {
+      response = e;
+      robot.log(e);
+      robot.log('Unable to add user to organization. :(');
+    }
   }
   
 };
