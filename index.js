46f4078b2d1e7802b0719bf278370a78305299ff
erick <erick@hotmail.ca>
2017-10-14 22:32:07 -0400

    Add termination if user is already part of org

--- index.js
+++ index.js
@@ -1,43 +1,35 @@
 // use sentry.io for exception tracking
-var Raven = require('raven');
-Raven.config(`https://${process.env.SENTRY_KEY}@sentry.io/228941`).install();
+var Raven = require('raven')
+Raven.config(`https://${process.env.SENTRY_KEY}@sentry.io/228941`).install()
 
 module.exports = (robot) => {
-  console.log('App started...')
+  robot.on('pull_request.closed', inviteMember)
 
-  robot.on('pull_request.closed', inviteMember);
-  
-  async function inviteMember(context) {
-      
-    const isMerged = context.payload.pull_request.merged;
-    const isOrg = context.payload.repository.owner.type === 'Organization';
+  async function inviteMember (context) {
+    const isMerged = context.payload.pull_request.merged
+    const isOrg = context.payload.repository.owner.type === 'Organization'
 
     if (context.isBot || !isMerged || !isOrg) {
       robot.log('This pull request belongs to a non-organization repo, has made by a bot user, or has been closed without merging. :(')
-      return;
+      return
     }
 
-    const memberPayload = {
+    const payload = {
       org: context.payload.repository.owner.login,
       username: context.payload.pull_request.user.login,
       role: 'member'
     }
 
-    let response;
+    // check if user is already part of organization
+    // the api call throws if user is not part of org
     try {
-      response = await context.github.orgs.addOrgMembership(memberPayload);
-
-      if (response.state === 'pending') {
-        robot.log('User is already in organization! :)')
-      } else {
-        robot.log('User has been added to the organization! :)');
-      }
-
-    } catch(e) {
-      response = e;
-      robot.log(e);
-      robot.log('Unable to add user to organization. :(');
+      await context.github.orgs.getOrgMembership(payload)
+      robot.log(`Cannot invite ${payload.username} because they already been invited to ${payload.org}!`)
+      return
+    } catch (e) {
+      // if user is not part of org, invite them
+      await context.github.orgs.addOrgMembership(payload)
     }
+
   }
-  
-};
+}
